e4728f71371f2551dd65da9f13f57c823ec227ac
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-03-16 14:24:55 -0400

    working on saving user city and state to database

--- models/User.js
+++ models/User.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, default: '', lowercase: true, required: true },
-  email: { type: String, unique: true, lowercase: true, required: true },
+  name: { type: String, lowercase: true, required: true },
+  email: { type: String, lowercase: true, unique: true, required: true },
+  city: { type: String, lowercase: true},
+  state: {
+    type: String,
+    lowercase: true,
+    minlength: 2,
+    maxlength: 2
+  },
   password: { type: String, required: true }
 });
 
--- routes/weather.js
+++ routes/weather.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-
-// let url = `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/${lat},${lon}`;
+const User = require('../models/User');
 
 router.get('/', (req, res) => {
-  fetch(`https://api.darksky.net/forecast/${process.env.SECRET_KEY}/42.3601,-71.0589?exclude=minutely,flags`)
-    .then(result => result.json())
-    .then(data => {
-      res.send(data)
-    })
-    .catch(err => res.send(err));
-  // if (req.isAuthenticated()) {
-  //   res.render('weather');
-  // } else {
-  //   res.send('Unauthorized');
-  // }
+  // const apiUrl = `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/${lat},${lon}`;
+  // const apiUrl = `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/37.8267,-122.4233`;
+  // const response = await fetch(apiUrl);
+  // const json = await response.json();
+  // response.json(json);
+
+  if (req.isAuthenticated()) {
+    fetch(
+      `https://api.weatherbit.io/v2.0/current?city=${req.user.city},${req.user.state}&key=${process.env.SECRET_KEY}`
+    )
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        return res.send(data);
+      })
+      .catch(err => res.send(err));
+    console.log(req.user);
+    // res.render('weather');
+  } else {
+    res.send('Unauthorized');
+  }
 });
 
 module.exports = router;
