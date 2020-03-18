9600a96ebb615ca0bbd19378ab252f8646038c7c
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-03-18 14:51:30 -0400

    working on leaflet and google geocode

--- routes/weather.js
+++ routes/weather.js
@@ -3,36 +3,24 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const User = require('../models/User');
 
-// router.get('/', (req, res) => {
-//   if (req.isAuthenticated()) {
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(position => {
-//     let lon = position.coords.longitude;
-//     let lat = position.coords.latitude;
-//     fetch(
-//       `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/${lat},${lon}`
-//     )
-//       .then(data => data.json())
-//       .then(data => console.log(data));
-//   });
-}
-//   } else {
-//     res.send('Unauthorized');
-//   }
-// });
-
-window.addEventListener('load', () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      let lon = position.coords.longitude;
-      let lat = position.coords.latitude;
-      fetch(
-        `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/${lat},${lon}`
-      )
-        .then(data => data.json())
-        .then(data => console.log(data));
-    });
+router.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.render('weather');
+  } else {
+    return res.send('Unauthorized');
   }
 });
+
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(position => {
+//       let lon = position.coords.longitude;
+//       let lat = position.coords.latitude;
+//       fetch(
+//         `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/${lat},${lon}`
+//       )
+//         .then(data => data.json())
+//         .then(data => console.log(data));
+//     });
+//   }
 
 module.exports = router;
