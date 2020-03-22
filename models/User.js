c04ae7bcbc2cde5cd160d58e49561758bf7547e0
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-03-22 13:10:19 -0400

    working on login / register views

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: { type: String, lowercase: true, required: true },
   email: { type: String, lowercase: true, unique: true, required: true },
+  address: { type: String, required: true },
   password: { type: String, required: true }
 });
 
