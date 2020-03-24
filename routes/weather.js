379f31485f55ff5ab7010262bc17620233f7c36e
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-03-24 11:14:55 -0400

    still working on geocoder

--- routes/weather.js
+++ routes/weather.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
+const NodeGeocoder = require('node-geocoder');
 const User = require('../models/User');
 
 router.get('/', (req, res) => {
@@ -22,5 +23,21 @@ router.get('/', (req, res) => {
 //         .then(data => console.log(data));
 //     });
 //   }
+
+// const geocoder = NodeGeocoder({
+//   provider: 'google',
+//   httpAdapter: 'https',
+//   apiKey: 'AIzaSyAm1sRVoUp6vLGR_jKeC-syToSND9_hIjk',
+//   formatter: null
+// });
+
+// geocoder
+//   .geocode('29 champs elysée paris')
+//   .then(function(res) {
+//     console.log(res);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
 
 module.exports = router;
