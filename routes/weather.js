70a16ec014cca8b396a42b152dccc459af6dc361
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-03-25 14:57:08 -0400

    working on leaflet popup

--- routes/weather.js
+++ routes/weather.js
@@ -1,41 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-// const NodeGeocoder = require('node-geocoder');
 require('dotenv').config();
-// const geocoder = NodeGeocoder({
-//   provider: 'google',
-//   httpAdapter: 'https',
-//   apiKey: process.env.GOOGLE_KEY,
-//   formatter: null
-// });
 
 router.get('/', (req, res) => {
   if (req.isAuthenticated()) {
-    // geocoder
-    //   .geocode(req.user.zip)
-    //   .then(data => {
-    //     let lat = data[0].latitude;
-    //     let lon = data[0].longitude;
-
-        fetch(
-          // `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/${lat},${lon}`
-          `https://api.weatherbit.io/v2.0/current?postal_code=${req.user.zip}&key=${process.env.SECRET_KEY2}`
-        )
-          .then(data => data.json())
-          .then(data => {
-            console.log(data);
-            let detail = {
-              lat: data.latitude,
-              lon: data.longitude,
-              summary: data.currently.summary,
-              icon: data.currently.icon,
-              temp: data.currently.temperature,
-              feelsLike: data.currently.apparentTemperature
-            };
-            res.render('weather', { detail });
-          })
-      // })
+    fetch(
+      `https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=${req.user.zip}&key=${process.env.SECRET_KEY}&units=I`
+    )
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        let items = {
+          lat: data.lat,
+          lon: data.lon,
+          icon: data.data[0].weather.icon,
+          city: data.city_name,
+          state: data.state_code,
+          time: data.data[0].timestamp_local,
+          temp: Math.round(data.data[0].temp),
+          appTemp: Math.round(data.data[0].app_temp)
+        };
+        return res.render('weather', { items });
+      })
       .catch(err => {
         console.log(err);
       });
