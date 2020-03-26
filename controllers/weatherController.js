c340553211bdd57f56b4251725053c56e9db2d3c
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-03-26 11:58:46 -0400

    moved get weather to controller

---
+++ controllers/weatherController.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+module.exports = {
+  getWeather: (req, res) => {
+    if (req.isAuthenticated()) {
+      fetch(
+        `https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=${req.user.zip}&key=${process.env.SECRET_KEY}&units=I`
+      )
+        .then(data => data.json())
+        .then(data => {
+          console.log(data);
+          let items = {
+            lat: data.lat,
+            lon: data.lon,
+            city: data.city_name,
+            state: data.state_code,
+            time: data.data[0].timestamp_local.slice(0, -9),
+            icon: data.data[0].weather.icon,
+            rain: data.data[0].pop,
+            wind: Math.round(data.data[0].wind_spd),
+            temp: Math.round(data.data[0].temp),
+            appTemp: Math.round(data.data[0].app_temp)
+          };
+          return res.render('weather', { items });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else {
+      return res.send('Unauthorized');
+    }
+  }
+};
--- routes/weather.js
+++ routes/weather.js
@@ -1,36 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
-require('dotenv').config();
+const weatherController = require('../controllers/weatherController');
 
-router.get('/', (req, res) => {
-  if (req.isAuthenticated()) {
-    fetch(
-      `https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=${req.user.zip}&key=${process.env.SECRET_KEY}&units=I`
-    )
-      .then(data => data.json())
-      .then(data => {
-        console.log(data);
-        let items = {
-          lat: data.lat,
-          lon: data.lon,
-          city: data.city_name,
-          state: data.state_code,
-          time: data.data[0].timestamp_local.slice(0, -9),
-          icon: data.data[0].weather.icon,
-          rain: data.data[0].pop,
-          wind: Math.round(data.data[0].wind_spd),
-          temp: Math.round(data.data[0].temp),
-          appTemp: Math.round(data.data[0].app_temp)
-        };
-        return res.render('weather', { items });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  } else {
-    return res.send('Unauthorized');
-  }
-});
+router.get('/', weatherController.getWeather);
 
 module.exports = router;
